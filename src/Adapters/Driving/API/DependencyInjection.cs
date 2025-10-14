6b26015b7070398e2cd2e0127e73085dbdf2f0d9
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2025-10-13 23:55:39 -0300

    Add API layer with endpoint extensions, result handling, and dependency injection
    Description:
    - Introduced `DependencyInjection` to set up CORS, problem details, and API-related configurations.
    - Added `EndpointExtensions` for registering and mapping endpoints dynamically.
    - Implemented `ResultExtensions` for handling `Result` operations in a fluent way.
    - Introduced `CustomResults` to standardize HTTP responses for `Result` handling.
    - Defined `IEndpoint` interface for routing abstraction.
    - Applied minor refactoring in `CustomerRepository` to simplify methods.
    - Added public ID generation in `MongoRepositoryBase` for entities.

---
+++ src/Adapters/Driving/API/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿namespace API;
+
+internal static class DependencyInjection
+{
+    // ReSharper disable once InconsistentNaming
+    public static IServiceCollection AddAPI(this IServiceCollection services)
+    {
+        services.AddCustomCors();
+        services.AddProblemDetails();
+        services.AddEndpointsApiExplorer();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomCors(this IServiceCollection services)
+    {
+        services.AddCors(x => x.AddDefaultPolicy(option =>
+            option.AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(_ => true)
+                .AllowCredentials()
+        ));
+
+        return services;
+    }
+}
---
+++ src/Adapters/Driving/API/Endpoints/IEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Endpoints;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
